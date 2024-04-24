77097272f1a91f19b73b1147fb67581c4a86f9d6
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-04-24 18:21:54 +0200

    Cleanup

--- Patches/InventoryScrollPatch.cs
+++ Patches/InventoryScrollPatch.cs
@@ -1,12 +1,4 @@
-﻿using Aki.Reflection.Patching;
-using EFT.UI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine.UI;
+﻿using System.Reflection;
 
 namespace UIFixes
 {
