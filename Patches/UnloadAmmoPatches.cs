a29361958e07174e1ea9d4025e42589e0d4e6356
Tyfon <29051038+tyfon7@users.noreply.github.com>
2024-07-26 01:26:45 -0700

    Use correct inventoryController in ammobox unload

--- Patches/UnloadAmmoPatches.cs
+++ Patches/UnloadAmmoPatches.cs
@@ -201,7 +201,7 @@ public static class UnloadAmmoPatches
 
             if (operation.Succeeded)
             {
-                tempController.DestroyItem(ammoBox);
+                inventoryController.DestroyItem(ammoBox);
             }
             else
             {
