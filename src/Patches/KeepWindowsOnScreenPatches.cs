8a3b31d2b2e5a0325c627dbc65c4264d0093570a
Tyfon <29051038+tyfon7@users.noreply.github.com>
2025-01-24 17:52:02 -0800

    prefer keeping top of window on screen

--- src/Patches/KeepWindowsOnScreenPatches.cs
+++ src/Patches/KeepWindowsOnScreenPatches.cs
@@ -2,9 +2,11 @@
 using EFT.UI;
 using HarmonyLib;
 using SPT.Reflection.Patching;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using UnityEngine;
 
 namespace UIFixes;
 
@@ -17,6 +19,8 @@ public static class KeepWindowsOnScreenPatches
         new KeepWindowOnScreenPatch(nameof(ItemUiContext.OpenInsuranceWindow)).Enable();
         new KeepWindowOnScreenPatch(nameof(ItemUiContext.OpenRepairWindow)).Enable();
         new KeepWindowOnScreenPatch(nameof(ItemUiContext.method_3)).Enable(); // grids
+
+        new KeepTopOnScreenPatch().Enable();
     }
 
     private static void FixNewestWindow(List<InputNode> windows)
@@ -36,5 +40,31 @@ public static class KeepWindowsOnScreenPatches
 
         [PatchPostfix]
         public static void Postfix(List<InputNode> ____children) => FixNewestWindow(____children);
+    }
+
+    // Duplicates the function but specifically checks top only.
+    // The existing code checks bottom before top, which prefers to keep bottom on screen
+    // We want the top on the screen
+    public class KeepTopOnScreenPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(GClass916), nameof(GClass916.CorrectPositionResolution), [typeof(RectTransform), typeof(RectTransform), typeof(MarginsStruct)]);
+        }
+
+        [PatchPostfix]
+        public static void Postfix(RectTransform transform, RectTransform referenceTransform, MarginsStruct margins)
+        {
+            MarginsStruct distanceToBorders = transform.GetDistanceToBorders(referenceTransform, margins);
+            if (distanceToBorders.Top.Negative())
+            {
+                Vector2 vector = transform.position;
+                vector.y += distanceToBorders.Top;
+                Vector2 vector2 = transform.GetTopLeftToPivotDelta() * transform.lossyScale;
+                Vector2 vector3 = vector + vector2;
+                Vector2 vector4 = new Vector2((float)Math.Round(vector3.x), (float)Math.Round(vector3.y));
+                transform.position = vector4 - vector2;
+            }
+        }
     }
 }
