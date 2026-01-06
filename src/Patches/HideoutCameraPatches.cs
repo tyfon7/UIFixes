21feb526b1ec9ff5d79c80d30ed324e8e9be2e17
Tyfon <29051038+tyfon7@users.noreply.github.com>
2026-01-05 23:51:01 -0800

    hideout zoom mouse out of app

--- src/Patches/HideoutCameraPatches.cs
+++ src/Patches/HideoutCameraPatches.cs
@@ -5,6 +5,7 @@ using EFT.UI;
 using EFT.UI.Screens;
 using HarmonyLib;
 using SPT.Reflection.Patching;
+using UnityEngine;
 
 namespace UIFixes;
 
@@ -13,6 +14,7 @@ public static class HideoutCameraPatches
     public static void Enable()
     {
         new HideoutCameraPatch().Enable();
+        new HideoutZoomPatch().Enable();
     }
 
     // Prevent the hideout camera from moving due to screen-edge when an area is selected or inventory is open
@@ -29,6 +31,27 @@ public static class HideoutCameraPatches
             return !__instance.AreaSelected &&
                 CurrentScreenSingletonClass.Instance?.CurrentScreenController?.ScreenType is EEftScreenType.Hideout &&
                 !Singleton<CommonUI>.Instance.ChatScreen.gameObject.activeInHierarchy;
+        }
+    }
+
+    // Stop hideout zoom from the mousewheel when the mouse is on another screen
+    public class HideoutZoomPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(HideoutCameraController), nameof(HideoutCameraController.Zoom));
+        }
+
+        [PatchPrefix]
+        public static bool Prefix()
+        {
+            // Unity reports the mouse position as on the edge when it leaves the game
+            if (Input.mousePosition.x <= 0 || Input.mousePosition.y <= 0 || Input.mousePosition.x >= Screen.width || Input.mousePosition.y >= Screen.height)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
