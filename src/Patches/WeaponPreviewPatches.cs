1485317db434f03f2c5b87130611616a8b05c002
7Bpencil <Edward.Ekb@yandex.ru>
2026-07-02 17:14:53 +0500

    fix camera clipping in modding screen and edit build screen

---
+++ src/Patches/WeaponPreviewPatches.cs
@@ -0,0 +1,30 @@
+using System.Reflection;
+using EFT.UI.WeaponModding;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+
+namespace UIFixes;
+
+public static class WeaponPreviewPatches
+{
+	public static void Enable()
+	{
+        new WeaponPreviewCameraNearClipPatch().Enable();
+	}
+
+	public class WeaponPreviewCameraNearClipPatch : ModulePatch
+	{
+        protected override MethodBase GetTargetMethod()
+        {
+			// this is called when WeaponPreview is opened and fully initialized,
+			// WeaponPreview is used both by weapon modding screen, edit build screen, and item overview
+            return AccessTools.Method(typeof(WeaponPreview.Class3271), nameof(WeaponPreview.Class3271.method_1));
+        }
+
+        [PatchPostfix]
+        public static void Postfix(WeaponPreview.Class3271 __instance)
+        {
+            __instance.weaponPreview_0.WeaponPreviewCamera.nearClipPlane = 0.01f;
+		}
+	}
+}
